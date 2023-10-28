ebabd5946874daf8f1bb8bf27b188c804fa580b9
Pablo Bobarini <50304993+PablissimoB@users.noreply.github.com>
2023-10-28 18:21:25 -0300

    TP1

---
+++ 1/product.js
@@ -0,0 +1,20 @@
+class Product{
+    constructor(title, description, price, thumbnail, code, stock){
+        this.id = Product.newId();
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.code = code;
+        this.stock = stock;
+    }
+    static newId() {
+        if (this.idIncrement) {
+            this.idIncrement++
+        } else {
+            this.idIncrement = 1
+        }
+        return this.idIncrement
+    }
+}
+module.exports = Product;
