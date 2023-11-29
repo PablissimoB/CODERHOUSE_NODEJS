c3c2e7f9090028563a574dd790f4f6502549c64e
Pablo Bobarini <50304993+PablissimoB@users.noreply.github.com>
2023-11-29 15:27:37 -0300

    Fixed main

--- 4/src/public/js/main.js
+++ 4/src/public/js/main.js
@@ -1,23 +1,40 @@
-function deleteProduct(id){
+async function reloadProducts(){
+    try{
+        const productsResponse = await fetch('http://localhost:4000/api/products/');
+        const products = await productsResponse.json();
+
+        product_list.innerHTML =`<h1> Lista de Productos </h1> <hr>`
+        products.forEach(element => {
+            product_list.innerHTML +=`
+            <div class="card text-bg-success mb-3" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">${element.title}</h5>
+                <p class="card-text">${element.description}</p>
+                <p class="card-text">precio : ${element.price} - stock: ${element.stock}</p>
+                <button onclick="deleteProduct('${element.id}');" " class="btn btn-danger"> delete</button>
+                </div>
+            </div>
+            `;
+        });
+    }
+    catch(error){
+        console.error('Error al cargar los productos:', error);
+    }
+
+}
+
+async function deleteProduct(id){
     let prods;
     if(confirm(`Desea borrar el producto ${id} ?`)){
         const endpointDelete = `http://localhost:4000/api/products/${id}`;
 
-        fetch(endpointDelete, {
-        method: 'DELETE'
-        })
-        .then(response => {
+        const response = await fetch(endpointDelete, {
+            method: 'DELETE'
+          });
             if (response.ok) {
-                //esto lo hago porque todavia no se como renderizar products en handlebars con nuevo get
-                setTimeout(() => {
-                    location.reload();
-                  }, 1200);
+                await reloadProducts();
             } else {
             console.error('Error al borrar el elemento:', response.statusText);
             }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
     }
 }
